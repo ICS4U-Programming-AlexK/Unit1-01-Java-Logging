76f6ff4323410e85dca552ed085b79c0a555e03a
Alex Kapajika <alex.kapajika.ntumba@stu.ocsb.ca>
2025-02-24 15:54:44 +0000

    Log Company

--- Logging.java
+++ Logging.java
@@ -12,30 +12,30 @@ public final class Logging {
      * @exception IllegalStateException
      * @see IllegalStateException
      */
-
     private Logging() {
         throw new IllegalStateException("Utility class");
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        //Defining the constants
         System.out.print("Enter Log length (0.25, 0.5 or 1) : ");
         final float LOG_LENGTH_1 = 0.25f;
         final float LOG_LENGTH_2 = 0.5f;
         final float LOG_LENGTH_3 = 1f;
         final int TRUCK_LIMIT = 1100;
         String strLogLength = scanner.nextLine();
-
+        //Trying to handle any error input
         try{
             float LogLength = Float.parseFloat(strLogLength);
             if ((LogLength == LOG_LENGTH_1) || (LogLength == LOG_LENGTH_2) || (LogLength == LOG_LENGTH_3)){
-                float LOGAMOUNT = TRUCK_LIMIT/20 * LogLength;
-                System.out.println("The truck will be able to take = " + LOGAMOUNT + " Km");
+                float LOGAMOUNT = TRUCK_LIMIT/20/LogLength;
+                System.out.println("The truck will be able to take = " + LOGAMOUNT + " Logs");
             } else {
                 System.out.println("This length is not part of the options" + LogLength);
                 return;
             }
+        //Catching the exception
         } catch (NumberFormatException error){
             System.out.println(error.getMessage());
         }
